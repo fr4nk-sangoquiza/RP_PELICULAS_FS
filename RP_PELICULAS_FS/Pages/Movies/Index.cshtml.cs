2caf788f7fe351c149e122c79e7f21aefb77c5e6
UsuarioFS <UsuarioFS@PC-FRANKLIN>
2021-02-20 00:55:05 +0100

    *

--- RP_PELICULAS_FS/Pages/Movies/Index.cshtml.cs
+++ RP_PELICULAS_FS/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RP_PELICULAS_FS.Data;
 using RP_PELICULAS_FS.Modelos;
@@ -21,9 +22,25 @@ namespace RP_PELICULAS_FS.Pages.Movies
 
         public IList<Movie> Movie { get;set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string valor_busqueda { get; set; }  //variable donde se guarda lo que introduce el usuario
+
+        public SelectList genero { get; set; }      //Variable donde tenemos la lista de los géneros
+
+        [BindProperty(SupportsGet = true)]
+        public string generoPelicula { get; set; }  //Variabale que guarda el género de la lista elegido por el usuario
+
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            var peliculas = from m in _context.Movie
+                            select m;
+            if (!string.IsNullOrEmpty(valor_busqueda))  // Si lo que se ha buscado no es nulo o vacío
+            {
+                peliculas = peliculas.Where(s => s.titulo.Contains(valor_busqueda));
+            }
+
+            //Movie = await _context.Movie.ToListAsync();
+            Movie = await peliculas.ToListAsync();     //Para mostrar solo la variable pelicula que contiene la búsqueda
         }
     }
 }
