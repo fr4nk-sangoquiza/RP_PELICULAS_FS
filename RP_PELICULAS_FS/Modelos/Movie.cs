ba491e09d59e0e2b3a37b06b9a889d57d1001543
UsuarioFS <UsuarioFS@PC-FRANKLIN>
2021-02-20 00:00:28 +0100

    *

--- RP_PELICULAS_FS/Modelos/Movie.cs
+++ RP_PELICULAS_FS/Modelos/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,14 +12,24 @@ namespace RP_PELICULAS_FS.Modelos
         // PROPIEDADDES O ATRIBUTOS DE LA NUEVA PÁGINA
 
         public int id { get; set; }
+
+        [Display(Name = "Título")]
         public string titulo { get; set; }
+
+        [Display(Name = "Género")]
         public string genero { get; set; }
+
+        [Display(Name = "Precio")]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal precio { get; set; }
 
-        [DataType(DataType.Date)]                // Acortamos la fecha. Esto se muestra en la web en formato fecha
-        public DateTime fechaLanzamiento { get; set; }      // Representa el día, la fecha, la hora y los segundos
-        
+        [DataType(DataType.Date)]                       // Acortamos la fecha. Esto se muestra en la web en formato fecha
+        [Display(Name = "Fecha de Lanzamiento")]        // atributo cambiamos el nombre del campo que va visualizar el usuario
+        public DateTime fechaLanzamiento { get; set; }  // Representa el día, la fecha, la hora y los segundos
+
+        [Display(Name = "Descripción")]
         public string descripcion { get; set; }
+
 
 
     }
